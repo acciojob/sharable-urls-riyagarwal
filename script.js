34318dce19400b7062bae36f9357609ad1349c75
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-09-07 15:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+const handleBtClick = () => {
+	let name = document.getElementById("name").value
+	let year = document.getElementById("year").value
+
+	document.getElementById("url").innerText = 
+		`https://localhost:8080/?name=${name}&year={year}`
+}
